e370a82d1fe4ef9bbd37d0751377476f05bfbf89
gvcortez1 <gvcortez1@gmail.com>
2022-08-07 00:04:10 -0300

    adicionando modelo Categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,9 +1,11 @@
 import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
 import { Observable } from "rxjs";
 
+import { Category } from "./pages/categories/shared/category.model"
+
 export class InMemoryDbService implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             {id_descricao: 1, nome: 'Moradia', descricao: 'Pagamento de Contas da Casa'}
             , {id_descricao: 2, nome: 'Saúde', descricao: 'Pagamento de Saúde e Remédios'}
             , {id_descricao: 3, nome: 'Lazer', descricao: 'Cinema, parques, praias, etc.'}
---
+++ src/app/pages/categories/shared/category.model.ts
@@ -0,0 +1,7 @@
+export class Category {
+    constructor(
+            public id_categoria?: number,
+            public nome?: string,
+            public descricao?: string
+    ) {}
+}
