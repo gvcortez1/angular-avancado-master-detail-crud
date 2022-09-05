09c1792cd8fabd2e1a0ad582740f5dea5a98e6df
gvcortez1 <gvcortez1@gmail.com>
2022-09-05 10:01:31 -0300

    reestruturando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDatabase } from './in-memory-database';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule,
     AppRoutingModule
-    , HttpClientModule
-    , HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
-    , BrowserAnimationsModule
+    , CoreModule
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
